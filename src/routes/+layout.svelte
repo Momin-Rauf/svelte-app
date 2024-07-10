8440116aa61d93678857d00c856e5138bfdd1689
Momin-Rauf <mrauf.bese21seecs@seecs.edu.pk>
2024-07-11 02:39:37 +0500

    tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+import Navbar from "$lib/Navbar.svelte";
+import '../app.css'
+</script>
+
+<Navbar/>
+<h1 class="underline bg-black text-white" >hello this is tailwind test</h1>
+<slot/>
+
+
+    
